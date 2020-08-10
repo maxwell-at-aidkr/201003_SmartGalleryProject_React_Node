b2785f974d69f4429f003370d5f1f4a6fa7286f3
MJbae <bmj5208@gmail.com>
2020-08-10 22:39:16 +0900

    feat: UserList.js 내 remove 버튼 추가

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -4,6 +4,7 @@ function User({ user }) {
   return (
     <div>
       <b>{user.username}</b> <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 }
@@ -12,7 +13,7 @@ function UserList({ users }) {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} />
+        <User user={user} key={user.id} onRemove={onRemove} />
       ))}
     </div>
   );
