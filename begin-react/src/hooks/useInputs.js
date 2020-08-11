39aed8feb917722499ea93289d8d716b4a382ba0
MJbae <bmj5208@gmail.com>
2020-08-12 07:44:44 +0900

    refactor: useInputs.js 에러 개선(type 부분 오탈자 수정)

--- begin-react/src/hooks/useInputs.js
+++ begin-react/src/hooks/useInputs.js
@@ -1,14 +1,38 @@
-import { useState, useCallback } from "react";
+import { useReducer, useCallback } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CHANGE_INPUT":
+      return {
+        ...state,
+        [action.name]: action.value,
+      };
+    case "RESET":
+      return Object.keys(state).reduce((acc, current) => {
+        acc[current] = "";
+        return acc;
+      }, {});
+    default:
+      return state;
+  }
+}
 
 function useInputs(initialForm) {
-  const [form, setForm] = useState(initialForm);
+  const [form, dispatch] = useReducer(reducer, initialForm);
 
   const onChange = useCallback((e) => {
     const { name, value } = e.target;
-    //TODO: form => 다음에 소괄호가 왜 들어갈까?
-    setForm((form) => ({ ...form, [name]: value }));
+    dispatch({
+      type: "CHANGE_INPUT",
+      name,
+      value,
+    });
   }, []);
-  const reset = useCallback(() => setForm(initialForm), [initialForm]);
+
+  const reset = useCallback(() => {
+    dispatch({ type: "RESET" });
+  }, []);
+
   return [form, onChange, reset];
 }
 
