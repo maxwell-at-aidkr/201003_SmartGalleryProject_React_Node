d30cbdeb1c6d52ad13c66a4bc5dfb4823460b508
MJbae <bmj5208@gmail.com>
2020-08-11 20:22:51 +0900

    feat: custom hook 예제 추가

---
+++ begin-react/src/hooks/useInputs.js
@@ -0,0 +1,15 @@
+import { useState, useCallback } from "react";
+
+function useInputs(initialForm) {
+  const [form, setForm] = useState(initialForm);
+
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    //TODO: form => 다음에 소괄호가 왜 들어갈까?
+    setForm((form) => ({ ...form, [name]: value }));
+  }, []);
+  const reset = useCallback(() => setForm(initialForm), [initialForm]);
+  return [form, onChange, reset];
+}
+
+export default useInputs;
