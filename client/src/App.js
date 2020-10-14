f9282bb295770dea984437d9980e130f818f3896
MJbae <bmj5208@gmail.com>
2020-10-14 15:55:00 +0900

    chore: set proxy server

--- client/src/App.js
+++ client/src/App.js
@@ -13,13 +13,6 @@ function App() {
   return (
 <Router>
       <div>
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
         <Route exact path="/" component={LandingPage} />
           <Route exact path="/login" component={LoginPage} />
