46597d3632ad04b524aaceac76cf7f06dbcd7f33
MJbae <bmj5208@gmail.com>
2020-10-15 14:08:38 +0900

    chore: WorkUploadPage 라우트 설정

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import LandingPage from "./components/views/LandingPage/LandingPage.js";
 import LoginPage from "./components/views/LoginPage/LoginPage.js";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage.js";
+import WorkUploadPage from "./components/views/WorkUploadPage/WorkUploadPage";
 import Auth from "./hoc/auth";
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          {/* TODO: 추후 관리자만 접속 가능하도록 수정 */}
+          <Route exact path="/work/upload" component={Auth(WorkUploadPage, true)} />
         </Switch>
       </div>
     </Router>
---
+++ client/src/components/views/WorkUploadPage/WorkUploadPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WorkUploadPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WorkUploadPage
