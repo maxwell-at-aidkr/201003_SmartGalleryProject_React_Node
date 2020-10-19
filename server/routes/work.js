6f23f9d1d654335017d4724e8d642b17ff686ae4
MJbae <bmj5208@gmail.com>
2020-10-19 22:08:42 +0900

    chore: 노드 서버 내 이미지 경로 설정 오류 해결

--- server/routes/work.js
+++ server/routes/work.js
@@ -38,19 +38,19 @@ router.post('/uploadfiles', (req, res) => {
   });
 });
 
-router.get('/getWorks', (req, res) => {
-  Work.find().exec((err, works) => {
-    if (err) return res.status(400).send(err);
-    res.status(200).json({ success: true, works });
-  });
-});
-
 router.post('/uploadWork', (req, res) => {
   const work = new Work(req.body);
 
   work.save((err, work) => {
     if (err) return res.status(400).json({ success: false, err });
     return res.status(200).json({ success: true });
+  });
+});
+
+router.get('/getWorks', (req, res) => {
+  Work.find().exec((err, works) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).json({ success: true, works });
   });
 });
 
