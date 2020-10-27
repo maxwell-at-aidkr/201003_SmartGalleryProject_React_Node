79b0e82b0e1af9e1cff77522bf368bf9846322e0
MJbae <bmj5208@gmail.com>
2020-10-27 21:29:20 +0900

    chore: npm version upgrade

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,4 +1,3 @@
 module.exports = {
-  mongoURI:
-    'mongodb+srv://manjin:abcd1234@boilerplate.dq0kk.mongodb.net/<dbname>?authSource=admin&replicaSet=atlas-9y2g7v-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true',
+  mongoURI: process.env.MONGO_URI,
 };
