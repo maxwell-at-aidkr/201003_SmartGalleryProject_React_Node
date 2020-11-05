526968e94ed6c57c579fdbd8d95e4093ca5a250a
MJbae <bmj5208@gmail.com>
2020-11-05 09:41:46 +0900

    chore: AWS EB S3 변경

--- server/routes/works.js
+++ server/routes/works.js
@@ -34,7 +34,7 @@ const s3 = new AWS.S3({
 let upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: 'elasticbeanstalk-ap-northeast-2-297348835951',
+    bucket: 'smartgallerystorage',
     key: function (req, file, cb) {
       cb(null, `${Date.now()}_${file.originalname}`);
     },
@@ -44,7 +44,7 @@ let upload = multer({
 
 router.post('/image', upload.single('file'), function (req, res, next) {
   let file = req.file;
-  let AWSfilePath = `https://elasticbeanstalk-ap-northeast-2-297348835951.s3.ap-northeast-2.amazonaws.com/${file.key}`;
+  let AWSfilePath = `https://smartgallerystorage.s3.ap-northeast-2.amazonaws.com/${file.key}`;
 
   let result = {
     success: true,
